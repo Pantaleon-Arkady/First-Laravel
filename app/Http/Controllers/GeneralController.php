26a42b06391a6023a7440413f4234583325443cd
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2025-12-01 17:49:27 +0800

    Controller/Class   - validated request method   - retrieved/posted input/data   - displayed input/data

--- app/Http/Controllers/GeneralController.php
+++ app/Http/Controllers/GeneralController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class GeneralController extends Controller
 {
-    //
+    public function choosePokemon(Request $request)
+    {
+        if ($request->isMethod('post')) {
+
+            $pokemon = $request->input('pokemon');
+
+            echo "youve chosen this pokemon: $pokemon";
+        } else {
+            echo 'request method not allowed';
+        }
+    }
 }

61c9ddd7c1509a4b96f217b38e0edef4e5309393
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2025-12-02 13:16:12 +0800

    landing page with a little bit of style

--- resources/views/initial.blade.php
+++ resources/views/initial.blade.php
@@ -8,15 +8,18 @@
     <title>First Laravel App</title>
     <link rel="stylesheet" href="/statics/bootstrap.min.css">
     <script src="/statics/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="/statics/style.css">
   </head>
-  <body class="p-2 bg-light">
-    <div class="container-fluid bg-primary text-white p-3 rounded">
-      <h1>Hello from your first template!</h1>
-      <div class="container-fluid text-black bg-white rounded"> 
+  <body class="landing_body">
+    <div class="landing_container bg-secondary text-white p-3 rounded">
+      <h1>Landing Page</h1>
+      <div class="text-black bg-white rounded"> 
         <h2>Pages List</h2>
-        <a href="/register">Register</a></br>
-        <a href="/homepage">Home</a>
-        <a href="/product-view">Product View</a>
+        <div class="link_div">
+            <a href="/register" class="btn btn-primary">Register</a>
+            <a href="/homepage" class="btn btn-primary">Home</a>
+            <a href="/product-view" class="btn btn-primary">Product View</a>
+        </div>
       </div>
     </div>
   </body>

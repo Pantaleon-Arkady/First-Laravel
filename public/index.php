869b70e36a50ad0faadd81ba4bb3f9a3b0ad2790
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2025-12-02 12:52:39 +0800

    series of uprecedented choices I accidentallay removed .env and vendor file, so had to clean up install and make a few changes locally to make things work again

--- public/index.php
+++ public/index.php
@@ -1,20 +1,48 @@
 <?php
 
-use Illuminate\Foundation\Application;
+use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
+/*
+|--------------------------------------------------------------------------
+| Check If The Application Is In Maintenance Mode
+|--------------------------------------------------------------------------
+*/
+
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+*/
+
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
+/*
+|--------------------------------------------------------------------------
+| Bootstrap Laravel Application
+|--------------------------------------------------------------------------
+*/
+
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+/*
+|--------------------------------------------------------------------------
+| Handle The Incoming Request via the HTTP Kernel
+|--------------------------------------------------------------------------
+*/
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);

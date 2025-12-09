fc60706405508bbf7901b12df011ab887bb22393
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2025-12-09 14:53:05 +0800

    create user eloquent

--- app/Http/Controllers/CrudUserController.php
+++ app/Http/Controllers/CrudUserController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class CrudUserController extends Controller
 {
-    public function userRegister()
+    public function userRegister(Request $request)
     {
+        $dataPosted = $request->validate([
+            'name' => ['required', 'min:5', 'max:20'],
+            'email' => 'required',
+            'password' => ['required', 'min:8', 'max:50']
+        ]);
+
+        $dataPosted['password'] = bcrypt($dataPosted['password']);
+
+        User::create($dataPosted);
+
         return 'Registered response string from controller';
     }
 }

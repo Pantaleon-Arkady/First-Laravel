bf51ae6be665110f1e0540a119c67502bc19abb3
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2025-12-12 15:21:18 +0800

    Create Post Form Data   - required both fields   - sanitized by strip_tags function - removing html and php tags   - checked the data for user_id retrieved by auth id - worked swell

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\GeneralController;
 
 class PostController extends Controller
 {
     public function createPost(Request $request)
     {
-        echo 'creating post';
+        $dataInput = $request->validate([
+            'title' => 'required',
+            'content' => 'required'
+        ]);
+
+        $dataInput['title'] = strip_tags($dataInput['title']);
+        $dataInput['content'] = strip_tags($dataInput['content']);
+        $dataInput['user_id'] = Auth::id();
+
+        GeneralController::fastPrint($dataInput);
     }
 }

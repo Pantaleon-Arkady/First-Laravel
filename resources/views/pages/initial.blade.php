b533454dc84791ceef6df2e6b2dd5c09e3156deb
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2025-12-16 14:45:11 +0800

    Proper layout trial   - made a main default file   - created a structure of head, main, and footer   - tried the capabilities with two home pages

---
+++ resources/views/pages/initial.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.default')    
+
+@section('header')
+    <span>initial</span>
+@endsection
+
+@section('body-content')
+    <span>initial body</span>
+@endsection
+
+@section('footer')
+    <span>initial footer</span>
+@endsection

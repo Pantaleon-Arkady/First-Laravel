cefdaa369044f1f5d40643eb9ee2cd4824d3edf2
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2025-12-16 13:35:35 +0800

    tried inline text resize

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -78,15 +78,18 @@
     @else
 
         <!-- Toggle Buttons -->
-        <div class="border border-gray-400 p-4 rounded-md flex gap-4 justify-center mb-6">
-            <button onclick="showRegister()"
-                class="px-4 py-2 bg-gray-800 text-white rounded hover:bg-gray-700 transition">
-                Register
-            </button>
-            <button onclick="showLogin()"
-                class="px-4 py-2 bg-gray-300 text-gray-900 rounded hover:bg-gray-400 transition">
-                Log in
-            </button>
+        <div class="border border-gray-400 p-4 rounded-md">
+            <h3 class="text-[30px] font-semibold">First Laravel App</h3>
+            <div class="flex gap-4 justify-center mb-6 mt-3">
+                <button onclick="showRegister()"
+                    class="px-4 py-2 bg-gray-800 text-white rounded hover:bg-gray-700 transition">
+                    Register
+                </button>
+                <button onclick="showLogin()"
+                    class="px-4 py-2 bg-gray-300 text-gray-900 rounded hover:bg-gray-400 transition">
+                    Log in
+                </button>
+            </div>
         </div>
 
         <!-- Register Form -->

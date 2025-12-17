3b0616f830c62717653ae603be534e1735e33ec5
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2025-12-17 13:42:43 +0800

    Signup Forms   - separated both register and login forms   - note!!! return proper errors

--- resources/views/partials/auth/signup.blade.php
+++ resources/views/partials/auth/signup.blade.php
@@ -13,46 +13,6 @@
 </div>
 
 <!-- Register Form -->
-<div class="form-div" id="registerForm">
-    <h3 class="text-xl font-semibold mb-3">Register</h3>
-    <form action="/user-register" method="POST" class="space-y-3">
-        @csrf
-        <input type="text" name="name" placeholder="Enter a username..."
-            class="input-field"/>
-
-        <input type="email" name="email" placeholder="Please enter your email..."
-            class="input-field"/>
-
-        <input type="password" name="password" placeholder="Create a password..."
-            class="input-field"/>
-
-        <button type="submit"
-                class="btn-secondary">
-            Register
-        </button>
-    </form>
-</div>
-
+    @include('partials.auth.forms.register')
 <!-- Login Form -->
-<div class="form-div" id="loginForm">
-    <h3 class="text-xl font-semibold mb-3">Log In</h3>
-
-    <form action="/user-login" method="POST" class="space-y-3">
-        @csrf
-
-        <input type="text" name="logName" value="{{ old('logName') }}" placeholder="Username..."
-            class="input-field"/>
-
-        <input type="password" name="logPassword" placeholder="Password..."
-            class="input-field"/>
-
-        <button type="submit"
-                class="btn-secondary">
-            Log In
-        </button>
-    </form>
-
-    @if ($errors->has('logName'))
-        <div class="mt-2 text-sm text-red-500">{{ $errors->first('logName') }}</div>
-    @endif
-</div>
+    @include('partials.auth.forms.login')
